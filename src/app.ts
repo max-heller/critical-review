b898e65139ad1d14db07b67c396777f5691d0f93
mxheller <heller.max1@gmail.com>
2020-03-29 13:53:06 -0400

    Typescript/express project setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
